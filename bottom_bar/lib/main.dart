7aa4b052cd86141aefd035bf67cc32bd8247a2d8
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2021-06-28 11:21:57 -0300

    Initial commit

---
+++ bottom_bar/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:bottom_bar/pages/pinterest_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bottom Bar',
+      home: PinterestPage(),
+    );
+  }
+}
